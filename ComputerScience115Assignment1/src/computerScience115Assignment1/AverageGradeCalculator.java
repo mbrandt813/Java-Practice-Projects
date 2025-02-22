a159576a0a98bc197425dacbe717d28b7cfd35d4
Myranda Brandt <mbrandt813@hotmail.com>
2025-02-21 19:36:20 -0500

    First step of project completed.

--- ComputerScience115Assignment1/src/computerScience115Assignment1/AverageGradeCalculator.java
+++ ComputerScience115Assignment1/src/computerScience115Assignment1/AverageGradeCalculator.java
@@ -1,5 +1,20 @@
 package computerScience115Assignment1;
 
+/*This program is for the assignment in CS-115.
+  Program Author: Myranda Brandt*/
+
+
 public class AverageGradeCalculator {
 
+	public static void main(String[] args) {
+	
+	/*The first step in the assignment is to print out the program 
+	 * specifications for the project*/
+	System.out.println("This program will create an average grade calculator.");
+	System.out.println("The user will enter up to ten quiz scores.");
+	System.out.println("The program will then calculate the average score.");
+	System.out.println("The grade letter of the the average score will be displayed.");
+		
+	}
+	
 }
