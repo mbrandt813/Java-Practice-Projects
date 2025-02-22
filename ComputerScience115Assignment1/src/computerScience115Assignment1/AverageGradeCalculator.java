33c3351296a374a09f70f039bd401242a7eaf154
Myranda Brandt <mbrandt813@hotmail.com>
2025-02-21 19:59:09 -0500

    Second step of project completed.

--- ComputerScience115Assignment1/src/computerScience115Assignment1/AverageGradeCalculator.java
+++ ComputerScience115Assignment1/src/computerScience115Assignment1/AverageGradeCalculator.java
@@ -3,6 +3,7 @@ package computerScience115Assignment1;
 /*This program is for the assignment in CS-115.
   Program Author: Myranda Brandt*/
 
+import java.util.Scanner;
 
 public class AverageGradeCalculator {
 
@@ -14,7 +15,19 @@ public class AverageGradeCalculator {
 	System.out.println("The user will enter up to ten quiz scores.");
 	System.out.println("The program will then calculate the average score.");
 	System.out.println("The grade letter of the the average score will be displayed.");
-		
+	Scanner grade = new Scanner(System.in);	
+	int gradeEntered = 0;
+	
+	while (gradeEntered != 999) {
+	System.out.println("Enter a grade from a quiz:");
+	gradeEntered = grade.nextInt();
+	    if (gradeEntered == 999) {
+	    	System.out.println("Thank you for submitting your grades!");
+	    	break;
+	    }
+	System.out.println("Enter 999 to quit.");
+	}
+	
 	}
 	
 }
