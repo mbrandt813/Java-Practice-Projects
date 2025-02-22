0240c30ef1f9bdb3660366296a62edce416ead9d
Myranda Brandt <mbrandt813@hotmail.com>
2025-02-22 11:26:30 -0500

    Completed fifth step in assignment.

--- ComputerScience115Assignment1/src/computerScience115Assignment1/AverageGradeCalculator.java
+++ ComputerScience115Assignment1/src/computerScience115Assignment1/AverageGradeCalculator.java
@@ -5,6 +5,7 @@ package computerScience115Assignment1;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 public class AverageGradeCalculator {
 
@@ -35,39 +36,61 @@ public class AverageGradeCalculator {
 	 * added to the array after the loop runs, so when the tenth value gets added it
 	 * will stop looping. */
 	while (gradeList.size() < 10) {
-		//Ask the user to enter a grade.
-	    System.out.println("Enter a grade from a quiz:");
-	    //When the user enters a grade assign it to the gradeEntered instance.
-	    gradeEntered = grade.nextInt();
-	    /*If the grade entered was the code for stopping the program, display a
-	     *display a message to the user letting them know the grade entering 
-	     *process has stopped and break out of the while loop.*/
+    /*It's possible the user could enter a letter for their grade, so I will use a try
+     * and catch to handle the exception that will be throw.*/
+	    try {
+	        //Ask the user to enter a grade.
+		    System.out.println("Enter a grade from a quiz:");
+	        //When the user enters a grade assign it to the gradeEntered instance.
+	        gradeEntered = grade.nextInt();
+	        /*If the grade entered was the code for stopping the program, display a
+	        *display a message to the user letting them know the grade entering 
+	        *process has stopped and break out of the while loop.*/
 	        if (gradeEntered == 999) {
 	    	    System.out.println("Thank you for submitting your grades!");
 	    	    break;
-	    /*If the user enters a grade higher than 100 OR less than 0 let them know that
-	     * is an invalid grade and do not add the grade to the array.*/
+	        /*If the user enters a grade higher than 100 OR less than 0 let them know that
+	        * is an invalid grade and do not add the grade to the array.*/
 	        } else if (gradeEntered > 100 || gradeEntered < 0){
 	        	System.out.println("Error: That is an invalid quiz grade, try again!");
 	        	continue;
 	        }
-	        
+	     //If the grade was valid, add it to the ArrayList
 	    gradeList.add(gradeEntered);
+	    //If the grade was not a whole number, ask them to try again.
+	    } catch (InputMismatchException e) {
+	    	System.out.println("Grades must be a whole number, try again!");
+	    	grade.nextLine();
+	    }
 	/*Now that at least one grade has been entered let the user know they can stop
 	 *entering grades by typing 999.*/ 
 	System.out.println("Enter 999 to quit.");
-	//Add the valid grade to the ArrayList.
-	System.out.println(gradeList.size());
 	}
 
-	/*The fourth step in the assignment is to print all grade that were entered.*/
+	/*The fourth step in the assignment is to print all grades that were entered. To 
+	 * do that I will loop through the array list and print out each grade.*/
 	for (int i = 0; i < gradeList.size(); i++) {
 		/*I want to display the number of the quiz in the print out but still use
 		the correct index number so I will create an instance to hold the quiz number*/
 		int quizNum = i + 1;
 		System.out.println("Quiz " + quizNum + " Grade: " + gradeList.get(i)); 
-		System.out.println(i);
 	}
+	/*The fifth step in the assignment is to display the average grade from all the
+	 * grades entered. To do that I will use a for each loop to move through the 
+	 * ArrayList.*/
+	//Make an instance to hold the sum of all grades.
+	double sumOfGrades = 0;
+	for (int allGrades : gradeList) {
+		//For each grade, add it to the sumOfGrades instance
+		sumOfGrades += allGrades;		
+	}
+	/*Now to find the average I will divide the sum of all grades by the number of
+	 *grades entered. I will use a double data type so I can round it appropriately*/
+	double averageOfGrades = sumOfGrades / gradeList.size();
+	double roundedAverageOfGrades = Math.round(averageOfGrades);
+	//Display the average grade to the user.
+	System.out.println("The average of your quiz grades is: " + roundedAverageOfGrades);
+	
 	}
 	
 }
